22546d238dbed1615b163f361c532a1d73d7c81b
BaconErie <baconerie0001@gmail.com>
2022-07-03 11:14:55 -0400

    Write startup.py

---
+++ startup.py
@@ -0,0 +1,15 @@
+# Code to run the first time you start up the bot
+# This will setup the database in the notherbot folder
+
+import sqlite3
+
+connection = sqlite3.connect('notherbot/bot-data.db')
+cursor = connection.cursor()
+
+cursor.execute('CREATE TABLE global_user_data (user_id int, key text, value text, type text)')
+cursor.execute('CREATE TABLE guild_user_data (guild_id int, user_id int, key text, value text, type text)')
+cursor.execute('CREATE TABLE guild_data (guild_id int, key text, value text, type text)')
+
+connection.commit()
+
+connection.close()
