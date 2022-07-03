15de0b7fe5b0892fcd14a4f5f2b3e43a2ae933ca
BaconErie <baconerie0001@gmail.com>
2022-07-03 11:05:48 -0400

    a

--- notherbot/main.py
+++ notherbot/main.py
@@ -11,4 +11,4 @@ bot = commands.Bot(intents=intents, owner_ids=[727548572542959754])
 
 bot.load_extension('cogs.automod')
 
-bot.run('OTkyOTM4MTM2NDIwMjI5MjIx.GpwT3T.LEjreE3VzT8P4u-RqFGUDpbk2W6icQOMAGUdxM')
+bot.run()
